b83e25f138f80fc5fade4c80d05520d70cef0315
Isaac Idibia <idibiaisaac3@gmail.com>
2023-01-12 13:24:46 +0100

    sanity cli type fix4

--- sanitymedium/sanity.config.ts
+++ sanitymedium/sanity.config.ts
@@ -1,34 +1,34 @@
-// import {defineConfig} from 'sanity'
-// import {deskTool} from 'sanity/desk'
-// import {visionTool} from '@sanity/vision'
-// import {schemaTypes} from './schemas'
-
-// export default defineConfig({
-//   name: 'default',
-//   title: 'sanitymedium',
-
-//   projectId: '169omu8q',
-//   dataset: 'production',
-
-//   plugins: [deskTool(), visionTool()],
-
-//   schema: {
-//     types: schemaTypes,
-//   },
-// })
-
-import {defineConfig, isDev} from 'sanity'
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
-
 
 export default defineConfig({
   name: 'default',
-  title: 'sanityexpress',
+  title: 'sanitymedium',
 
   projectId: '169omu8q',
   dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
   },
 })
+
+// import {defineConfig, isDev} from 'sanity'
+// import {schemaTypes} from './schemas'
+
+
+// export default defineConfig({
+//   name: 'default',
+//   title: 'sanityexpress',
+
+//   projectId: '169omu8q',
+//   dataset: 'production',
+
+//   schema: {
+//     types: schemaTypes,
+//   },
+// })
